ebf81d5386d8abde2573d6e9c2e862b967b63e70
Aseer KT <aseermiqdad@gmail.com>
2021-08-25 07:51:54 +0530

    deps: update apollo server v3.2

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,69 @@
 import 'reflect-metadata';
+import http from 'http';
+import path from 'path';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { execute, subscribe } from 'graphql';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
 import { createConnection } from 'typeorm';
+import {
+  ApolloServerPluginLandingPageProductionDefault,
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} from 'apollo-server-core';
 import { errorLog, successLog } from './utils/chalkLogs';
-import createApp from './app';
+import { __prod__ } from './constants';
+import { buildSchema } from 'type-graphql';
+import createUserLoader from './dataloaders/userLoader';
+import createMemberLoader from './dataloaders/memberLoader';
 
 async function startServer() {
   await createConnection();
-  const { app } = await createApp();
+  const app = express();
+
+  const schema = await buildSchema({
+    resolvers: [path.join(__dirname, './resolvers/*.{j,t}s')],
+  });
+
+  const httpServer = http.createServer(app);
+  const subscriptionServer = SubscriptionServer.create(
+    {
+      schema,
+      execute,
+      subscribe,
+    },
+    {
+      server: httpServer,
+      path: '/graphql',
+    }
+  );
+  const apolloServer = new ApolloServer({
+    schema,
+    context: ({ req, res }) => ({
+      req,
+      res,
+      userLoader: createUserLoader(),
+      memberLoader: createMemberLoader(),
+    }),
+    plugins: [
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              subscriptionServer.close();
+            },
+          };
+        },
+      },
+      __prod__
+        ? ApolloServerPluginLandingPageProductionDefault
+        : ApolloServerPluginLandingPageGraphQLPlayground,
+    ],
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
 
   const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () =>
+  httpServer.listen(PORT, () =>
     console.log(successLog(`GraphQL API: http://localhost:${PORT}/graphql`))
   );
 }
