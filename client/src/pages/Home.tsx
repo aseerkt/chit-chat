fc4c2903e111c08490930d1e5c0451dd60403021
Aseer KT <aseermiqdad@gmail.com>
2021-10-25 20:46:30 +0530

    style: homepage

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,49 @@
+import { Button } from '@chakra-ui/button';
+import { Image } from '@chakra-ui/image';
+import { Flex, Grid, HStack, Text } from '@chakra-ui/layout';
 import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to Apollo DM</h1>
-      <Link to='/register'>Register</Link>
-      <Link to='/login'>Login</Link>
-    </div>
+    <Grid
+      h='100vh'
+      maxW='6xl'
+      mx='auto'
+      p='5'
+      align='center'
+      gap='5'
+      gridTemplateColumns={{ base: '1fr', sm: '1fr 1fr' }}
+    >
+      <Flex justify='center' align='center'>
+        <Image src='/undraw_chat_home.svg' w='80%' objectFit='contain' />
+      </Flex>
+      <Flex
+        direction='column'
+        align={{ base: 'center', sm: 'flex-end' }}
+        py='10'
+        justify='center'
+        textAlign='right'
+      >
+        <Text color='teal' fontSize='5xl' mb='3' fontWeight='bolder'>
+          ChitChat
+        </Text>
+        <Text color='gray' mb='2' fontSize='xl'>
+          Any easy way to chat among friends and groups
+        </Text>
+        <HStack spacing={3}>
+          <Link to='/register'>
+            <Button size='lg' variant='outline' colorScheme='teal'>
+              Register
+            </Button>
+          </Link>
+          <Link to='/login'>
+            <Button size='lg' colorScheme='teal'>
+              Login
+            </Button>
+          </Link>
+        </HStack>
+      </Flex>
+    </Grid>
   );
 }
 
